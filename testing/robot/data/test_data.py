d4a61fdd9b05680dbffbef4cacecd22921d7778d
Tomas Cechvala <tcechval@cisco.com>
2017-02-10 15:48:05 +0100

    WIP sec groups
    Change-Id: I7b73f7ff22bb3fc59c5e873818bdb5d5ad88c12d
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- testing/robot/data/test_data.py
+++ testing/robot/data/test_data.py
@@ -12,6 +12,8 @@ import uuid
 run_uuid = str(uuid.uuid4())
 network_name = 'fds_smoke_network_' + run_uuid
 subnet_name = 'fds_smoke_subnet_' + run_uuid
+sg_client = 'client'
+sg_server = 'server'
 vm1_name = 'fds_smoke_vm1_' + run_uuid
 vm1_address = '192.168.10.5'
 vm2_name = 'fds_smoke_vm2_' + run_uuid
@@ -21,7 +23,9 @@ port2_name = 'fds_smoke_port2_' + run_uuid
 subnet_cidr = '192.168.10.0/24'
 vm_flavor = 'm1.small'
 vm_image = 'cirros-0.3.3'
-userdata1 = "#!/bin/sh\n\nsudo ip a add {}/24 dev eth0\n".format(vm1_address)
+userdata1 = "#!/bin/sh\n\nsudo ip a add {}/24 dev eth0\n echo 'success' > f1\n sudo nc -l -p 80 < 'f1'\n".format(vm1_address)
 userdata2 = "#!/bin/sh\n\nsudo ip a add {}/24 dev eth0\nwhile true; do\n ping -c 1 {} 2>&1 >/dev/null\n " \
             "RES=$?\n if [ \"Z$RES\" = \"Z0\" ] ; then\n  echo 'ping PASSED'\n break\n else\n  echo " \
-            "'ping FAILED'\n fi\n sleep 1\ndone\n".format(vm2_address, vm1_address)
+            "'ping FAILED'\n fi\n sleep 1\ndone\n\nwhile true; do\n curl -m 5 {} 2>&1 >/dev/null\n " \
+            "RES=$?\n if [ \"Z$RES\" = \"Z0\" ] ; then\n  echo 'CURLPASS'\n else\n  echo " \
+            "'CURLFAIL'\n fi\n sleep 1\ndone\n".format(vm2_address, vm1_address, vm1_address)
