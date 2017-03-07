97f7ea76cc16038227e0c694ccbe821ed5a48186
Tomas Cechvala <tcechval@cisco.com>
2017-03-07 12:03:11 +0100

    Improving robot smoke test
    Suite setup modified:
     - flavor is checked and created if it does not exist
     - image is checked and created if it does not exist
    
    Change-Id: I0254827034fcb2e1a7f5f0983b5a5bad29eada43
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- testing/robot/lib/FDSLibrary.py
+++ testing/robot/lib/FDSLibrary.py
@@ -7,6 +7,9 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+from keystoneauth1 import loading
+from keystoneauth1 import session
+from glanceclient import client as glance
 from neutronclient.v2_0 import client as neutron
 from novaclient import client as nova
 from novaclient.exceptions import NotFound
@@ -15,8 +18,15 @@ import datetime
 import os
 import subprocess
 
+
 class FDSLibrary():
     def __init__(self):
+        self.glance_client = glance.Client('2', session=session.Session(
+            auth=loading.get_plugin_loader('password').load_from_options(auth_url=os.getenv('OS_AUTH_URL'),
+                                                                         username=os.getenv('OS_USERNAME'),
+                                                                         password=os.getenv('OS_PASSWORD'),
+                                                                         project_id=os.getenv('OS_PROJECT_ID'))))
+
         self.neutron_client = neutron.Client(username=os.getenv('OS_USERNAME'),
                                              password=os.getenv('OS_PASSWORD'),
                                              tenant_name=os.getenv('OS_TENANT_NAME'),
@@ -32,9 +42,24 @@ class FDSLibrary():
         flavor_list_names = [x.name for x in self.nova_client.flavors.list()]
         return flavor in flavor_list_names
 
+    def create_flavor(self, name, ram, vcpus="1", disk="0"):
+        response = self.nova_client.flavors.create(name, ram, vcpus, disk)
+        return response
+
     def check_image_exists(self, image):
-        image_list_names = [x.name for x in self.nova_client.images.list()]
+        image_list_names = [x.name for x in self.glance_client.images.list()]
         return image in image_list_names
+
+    def create_image(self, image_name, file_path, disk="qcow2",
+                     container="bare", public="public", property="hw_mem_page_size=large"):
+        image = self.glance_client.images.create(name=image_name,
+                                                 visibility=public,
+                                                 disk_format=disk,
+                                                 container_format=container,
+                                                 property=property)
+        with open(file_path) as image_data:
+            glance_client.images.upload(image.id, image_data)
+        return image.id
 
     def create_network(self, name):
         body = {'network': {'name': name}}
