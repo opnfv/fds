a331dc14310021cea3991172d1aa934dfa7195e2
juraj.linkes <jlinkes@cisco.com>
2017-05-18 10:25:43 +0200

    Added script for resource cleanup
    This script can be either run from jumphost or overcloud nodes.
    If run from jumphost, it will clean all resources on all nodes as well
    as openstack resources.
    If run from an overcloud node, it will clean resources on that node.
    The script supports whitelisting and exclusion of resources to be
    cleaned.
    
    Change-Id: I46c937dc31a1ed4b9be1d641183c9cc3b42bbb2d
    Signed-off-by: juraj.linkes <jlinkes@cisco.com>

--- scripts/mount_vpp_into_odl.sh
+++ scripts/mount_vpp_into_odl.sh
@@ -5,7 +5,7 @@ display_usage() {
   exit 85
 }
 
-. $(dirname $0)/variables.sh
+. $(dirname $0)/lib.sh
 
 if [  $# -lt 3 ]
 then
@@ -14,17 +14,17 @@ exit 1
 fi
 
 odl_ip=$1
-vpp_host=$2
-vpp_ip=$3
+hc_host=$2
+hc_ip=$3
 
 post_data='{"node" : [
-{"node-id":"'$vpp_host'",
-"netconf-node-topology:host":"'$vpp_ip'",
-"netconf-node-topology:port":"'$vpp_port'",
+{"node-id":"'$hc_host'",
+"netconf-node-topology:host":"'$hc_ip'",
+"netconf-node-topology:port":"'$hc_netconf_port'",
 "netconf-node-topology:tcp-only":false,
 "netconf-node-topology:keepalive-delay":0,
-"netconf-node-topology:username":"'$vpp_username'",
-"netconf-node-topology:password":"'$vpp_password'",
+"netconf-node-topology:username":"'$hc_username'",
+"netconf-node-topology:password":"'$hc_password'",
 "netconf-node-topology:connection-timeout-millis":10000,
 "netconf-node-topology:default-request-timeout-millis":10000,
 "netconf-node-topology:max-connection-attempts":10,
