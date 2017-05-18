cf520c638f407055d781942c8d2bab80e6929da7
juraj.linkes <jlinkes@cisco.com>
2017-05-18 10:25:43 +0200

    Added script for resource cleanup
    This script can be either run from jumphost or overcloud nodes.
    If run from jumphost, it will clean all resources on all nodes as well
    as openstack resources.
    If run from an overcloud node, it will clean resources on that node.
    The script supports whitelisting and exclusion of resources to be
    cleaned.
    
    Change-Id: I46c937dc31a1ed4b9be1d641183c9cc3b42bbb2d
    Signed-off-by: juraj.linkes <jlinkes@cisco.com>

--- scripts/remount_vpp_into_odl.sh
+++ scripts/remount_vpp_into_odl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir=$(dirname $0)
-. $script_dir/variables.sh
+. $script_dir/lib.sh
 
 overcloud_node_ips=$(grep -E "$NODE_PATTERN-[^-]+-[0-9]" /etc/hosts | grep -Eo '([0-9]{1,3}.){3}[0-9]{1,3}')
 for overcloud_node_ip in $overcloud_node_ips
