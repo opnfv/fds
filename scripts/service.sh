bfaea474140ed9ec34c4a29526e61f39b1c49760
juraj.linkes <jlinkes@cisco.com>
2017-06-03 11:25:51 +0200

    Added script for odl installation
    Also refactored part of code
    
    Change-Id: I4e479d538b03d17c7b460d04ef0bdbd980d39787
    Signed-off-by: juraj.linkes <jlinkes@cisco.com>

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+. $(dirname "$0")/lib.sh
 if [[ $# -ne 2 ]]
 then
     echo "Must have at least two arguments. The first is name of the service and the second is the action to be done with the service."
     exit 1
 fi
-for odl_hostname in `grep -Eo 'overcloud-controller-[0-9]' /etc/hosts`
-do
-    echo "Executing service $1 $2 on $odl_hostname"
-    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $odl_hostname service $1 $2 2> /dev/null
-done
+exec_on_controllers service $1 $2
